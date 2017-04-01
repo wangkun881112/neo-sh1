087eeda144cd6754c92ed67bd7476e019397568f
wangkun <wangkun@fenqi.im>
2017-04-01 11:30:45 +0800

    Origin pull

---
+++ demo/src/main/java/main/Main_CreateUUID.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.UUID;
+
+public class Main_CreateUUID {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3600; i++) {
+            System.out.println(UUID.randomUUID().toString().toUpperCase());
+        }
+    }
+}
---
+++ demo/src/main/java/main/TestSubString.java
@@ -0,0 +1,11 @@
+package main;
+
+public class TestSubString {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("1");
+        System.out.println(sb.toString().substring(1));
+        
+    }
+
+}
